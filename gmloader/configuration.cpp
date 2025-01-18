472cf4c0810a464d37f9125e5e1c9c9ab513d339
JohnnyonFlame <johnnyonflame@hotmail.com>
2025-01-18 19:21:32 -0300

    show_config is not a static method anymore.

--- gmloader/configuration.cpp
+++ gmloader/configuration.cpp
@@ -45,10 +45,10 @@ int gml_config::parse_file(const char* path) {
 }
 
 void gml_config::show_config(){
-    printf("config: save_dir = %s\n",gmloader_config.save_dir.c_str());
-    printf("config: apk_path = %s\n",gmloader_config.apk_path.c_str());
-    printf("config: show_cursor = %d\n",gmloader_config.show_cursor);
-    printf("config: disable_controller = %d\n",gmloader_config.disable_controller);
-    printf("config: disable_depth = %d\n",gmloader_config.disable_depth);
-    printf("config: force_platform = %s\n",gmloader_config.force_platform.c_str());
+    printf("config: save_dir = %s\n", save_dir.c_str());
+    printf("config: apk_path = %s\n", apk_path.c_str());
+    printf("config: show_cursor = %d\n", show_cursor);
+    printf("config: disable_controller = %d\n", disable_controller);
+    printf("config: disable_depth = %d\n", disable_depth);
+    printf("config: force_platform = %s\n", force_platform.c_str());
 }
